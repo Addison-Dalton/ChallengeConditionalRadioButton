ae615feff39557e427acd95d7463c11e67a73299
Addison Dalton <zarch14@gmail.com>
2018-01-30 11:14:29 -0500

    added default project files and functionality to meet challenge criteria

---
+++ ChallengeConditionalRadioButton/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>Your Note Taking Preferences</p>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:RadioButton ID="radioPencil" Text="Pencil" GroupName="noteTakingPref" runat="server" />
+                <br />
+                <asp:RadioButton ID="radioPen" Text="Pen" GroupName="noteTakingPref" runat="server" /> 
+                <br />
+                <asp:RadioButton ID="radioPhone" Text="Phone" GroupName="noteTakingPref" runat="server" /> 
+                <br />
+                <asp:RadioButton ID="radioTablet" Text="Tablet" GroupName="noteTakingPref" runat="server" /> 
+            </div>
+            <br />
+            <asp:Image ID="selectionImage" ImageUrl="" runat="server" />
+            <br />
+            <asp:Button ID="selectionBtn" Text="OK" runat="server" OnClick="SelectionBtn_Click"  />
+            <br />
+            <br />
+            <asp:Label ID="selectionResult" Text="" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SelectionBtn_Click(object sender, EventArgs e)
+        {
+            if (radioPencil.Checked)
+            {
+                selectionImage.ImageUrl = "~/Assets/pencil.png";
+                selectionResult.Text = "You selected Pencil.";
+
+            }
+            else if (radioPen.Checked)
+            {
+                selectionImage.ImageUrl = "~/Assets/pen.png";
+                selectionResult.Text = "You selected Pen.";
+            }
+            else if (radioPhone.Checked)
+            {
+                selectionImage.ImageUrl = "~/Assets/phone.png";
+                selectionResult.Text = "You selected Phone.";
+            }
+            else if (radioTablet.Checked)
+            {
+                selectionImage.ImageUrl = "~/Assets/tablet.png";
+                selectionResult.Text = "You selected Tablet.";
+            }
+            else
+            {
+                selectionResult.Text = "Please select an option.";
+            }
+        }
+    }
+}
